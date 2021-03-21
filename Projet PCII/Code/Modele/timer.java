d583d6b6ffd8feb16460bbec24e8a72bf1a8b05c
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2021-03-21 21:47:57 +0100

    Update timer.java

--- Projet PCII/Code/Modele/timer.java
+++ Projet PCII/Code/Modele/timer.java
@@ -1,2 +1,35 @@
 import java.time.Duration;
 import java.time.Instant;
+
+
+
+public class MyTimer {
+
+    /** debut timer**/
+    private Instant debutTimer;
+
+    /**Le temps mesuré par le timer **/
+    private long time;
+
+    /**Le debut de la pause**/
+    private Instant debutPause;
+
+    /** vérification si timer Est en pause**/
+    private boolean pause;
+
+
+    /** crée un timer qui se déclenche direct à son initialisation */
+    public MyTimer(long sec){
+        this.startTimer = Instant.now();
+        this.time = sec;
+        this.pause = false;
+    }
+  
+    /** fonction pause qui mets en pause le timer*/
+  
+    public void pause(){
+        this.pause = true;
+        this.startPause = Instant.now();
+    }
+
+  
