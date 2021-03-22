9fd0ee2080efa7ffd601b84ac525e182dc964ef4
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2021-03-22 17:17:14 +0100

    Update timer.java

--- Projet PCII/Code/Modele/timer.java
+++ Projet PCII/Code/Modele/timer.java
@@ -65,4 +65,14 @@ public class MyTimer {
         }
     }
     
+    public boolean timerOver(){
+        duration duration;
+        if(pause){
+            d = Duration.between(this.debutTimer, this.debutPause);
+        } else {
+            d = Duration.between(this.debutTimer, Instant.now());
+        }
+        return duration.toSeconds() >= this.time;
+    
+    }
   
