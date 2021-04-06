45f5e287609b67671243f80ca37b78cc3a88bda5
Nelly <nellylam94@gmail.com>
2021-04-07 01:51:52 +0200

    update files without warnings

--- Projet PCII/Code/Vue/AffichageFin.java
+++ Projet PCII/Code/Vue/AffichageFin.java
@@ -3,13 +3,10 @@ package Vue;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
--- Projet PCII/Code/Vue/AffichageRoute.java
+++ Projet PCII/Code/Vue/AffichageRoute.java
@@ -3,7 +3,6 @@ package Vue;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Point;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -43,12 +42,13 @@ public class AffichageRoute extends JPanel implements Observer{
 	public void setTemps(AvanceeTemps tps) { this.temps = tps; }
 	
 	
-	
+	/*
 	private void dessinerEcranPause(Graphics g){
 		g.setColor(Color.RED);
 		g.fillRect(0,0,AffichageJeu.LARGAFFICHAGE, AffichageJeu.HAUTAFFICHAGE);
 		g.drawString("Pause (appuyer sur espace pour reprendre la partie)", 400,300);
 	}
+	*/
 	
 
 	public void paint(Graphics g) {
