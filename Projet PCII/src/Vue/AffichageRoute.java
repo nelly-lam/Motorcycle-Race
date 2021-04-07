e4ef691767b284c7e3ed44bc586f13acd06802cc
Nelly <nellylam94@gmail.com>
2021-04-07 21:45:40 +0200

    add antialiasing

--- Projet PCII/src/Vue/AffichageRoute.java
+++ Projet PCII/src/Vue/AffichageRoute.java
@@ -2,10 +2,13 @@ package Vue;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import Controleur.AvanceeTemps;
@@ -94,7 +97,7 @@ public class AffichageRoute extends JPanel implements Observer{
 	 */
     public void paintMoto(Graphics g) throws IOException {
     	g.setColor(Color.blue);
-		Image img1 = ImageIO.read(new File("./src/Images/moto.png")).getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+		Image img1 = ImageIO.read(new File("src/Images/moto.png")).getScaledInstance(30, 30, Image.SCALE_DEFAULT);
 		g.drawRect((int) this.getMoto().getHautGauche().getX(), (int) this.getMoto().getHautGauche().getY(),
 					(int) this.getMoto().getBasDroit().getX() - (int) this.getMoto().getHautGauche().getX(), (int) this.getMoto().getBasDroit().getY() - (int) this.getMoto().getHautGauche().getY());
 		g.drawImage(img1, this.getMoto().getPositionX() - 15, this.getMoto().getPositionY() - 15, this);
@@ -134,7 +137,8 @@ public class AffichageRoute extends JPanel implements Observer{
     	}
     	
     	g.fillPolygon(pointsX, pointsY, pointsX.length);
-    	
+    	((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
 
     }
     
@@ -147,8 +151,9 @@ public class AffichageRoute extends JPanel implements Observer{
     	//g.setColor(Color.blue);
     	//g.fillRect(0, 0, AffichageJeu.LARGAFFICHAGE, Route.POSITIONHORIZON);
     	//g.drawLine( 0, Modele.Route.POSITIONHORIZON, Vue.AffichageJeu.LARGAFFICHAGE, Modele.Route.POSITIONHORIZON);
-
-		Image img = ImageIO.read(new File("./src/Images/fuji.png")).getScaledInstance(AffichageJeu.LARGAFFICHAGE, Route.POSITIONHORIZON, Image.SCALE_DEFAULT);
+    	
+    	//Image img = new Image(getClass().getResource("src/Images/fuji.png"));
+		Image img = ImageIO.read(new File("src/Images/fuji.png")).getScaledInstance(AffichageJeu.LARGAFFICHAGE, Route.POSITIONHORIZON, Image.SCALE_DEFAULT);
 		g.drawImage(img, 0, 0, this);
     }
 
@@ -161,7 +166,7 @@ public class AffichageRoute extends JPanel implements Observer{
     	g.setColor(Color.red);
     	//Image img;
     	for(int i = 0; i < this.getRoute().getListeObstacles().size(); i++) {
-    		//img = ImageIO.read(new File("./src/Images/cerisier.png")).getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+    		//img = ImageIO.read(new File("src/Images/cerisier.png")).getScaledInstance(30, 30, Image.SCALE_DEFAULT);
     		//g.drawImage(img, (int) this.getRoute().getListeObstacles().get(i).getX(), (int) this.getRoute().getListeObstacles().get(i).getY(), this);
     		g.drawString("X", (int) this.getRoute().getListeObstacles().get(i).getX(), (int) this.getRoute().getListeObstacles().get(i).getY());
     	}
